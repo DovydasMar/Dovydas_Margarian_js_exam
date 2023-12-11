cb207eb97738bd781e1bb34a7c93d486eff04c9d
DovydasMar <mardovydas@gmail.com>
2023-12-11 09:39:53 +0200

    task6 done

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,32 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+getUserAverageAge(users);
+function getUserAverageAge(userArr) {
+  let ageOfUsers = 0;
+  userArr.forEach((userObj) => {
+    ageOfUsers += userObj.age;
+  });
+  // console.log(ageOfUsers);
+  const averageAge = ageOfUsers / userArr.length;
+  // console.log(averageAge);
+}
+
+getUsersNames(users);
+function getUsersNames(userArr) {
+  const userNames = userArr.map((userObj) => userObj.name);
+  // userArr.forEach((userObj) => {
+  //   userNames.push(userObj.name);
+  // });
+  console.log(userNames);
+}
