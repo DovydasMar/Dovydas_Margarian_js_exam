b12fb058a514b5f1724900869f9aafa96b261151
DovydasMar <mardovydas@gmail.com>
2023-12-11 10:16:16 +0200

    task 8

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,33 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  a;
+  b;
+  constructor(aArg, bArg) {
+    this.a = aArg;
+    this.b = bArg;
+  }
+  sum() {
+    // console.log("sum =", this.a + this.b);
+    return this.a + this.b;
+  }
+  subtraction() {
+    // console.log("minus =", this.a - this.b);
+    return this.a - this.b;
+  }
+  multiplication() {
+    // console.log("sandauga =", this.a * this.b);
+    return this.a * this.b;
+  }
+  division() {
+    // console.log("dalyba =", this.a / this.b);
+    return this.a / this.b;
+  }
+}
+
+const calculatorius = new Calculator(2, 5);
+calculatorius.sum();
+calculatorius.subtraction();
+calculatorius.multiplication();
+calculatorius.division();
